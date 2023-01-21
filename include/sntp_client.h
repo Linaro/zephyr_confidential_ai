a6cecf33f9a39540df850db7207982ed26b47ce6
Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>
2023-01-21 18:01:12 +0000

    Sample: add SNTP client support
    Add sntp client implementation to connect to SNTP server to get
    real-time in seconds since 1st Jan 1970 and also regularly sync SNTP
    timestamp on 24hours.
    
    Signed-off-by: Rajkumar Kanagaraj <rajkumar.kanagaraj@linaro.org>

---
+++ include/sntp_client.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2023 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/net/sntp.h>
+#include <time.h>
+
+/**
+ * @brief Initialize the SNTP client and
+ * store elapsed real time since the system booted in
+ * milliseconds from 1st Jan 1970.
+ */
+void sntp_client_init();
+
+/**
+ * @brief Get the time from SNTP server and resync the time base.
+ */
+void sntp_resync_time();
+
+/**
+ * @brief Utility helps to get the current date and time.
+ *
+ * @param info   Pointer to store the date and time info to time
+ * management structure.
+ */
+void get_current_date_time(struct tm *info);
