56519b540e132e1d28a85ca068921858e33c40ca
David Brown <david.brown@linaro.org>
2023-03-06 16:13:37 -0700

    flow: Implement cose_encrypt
    Implement COSE_encrypt support, ensuring that the result can be decoded by the
    decrypt function. The resulting packet has a minimum of information in it, and
    will need to gain support for key id's and possibly session identifier
    information.
    
    Signed-off-by: David Brown <david.brown@linaro.org>

--- tools/flow/src/keys.rs
+++ tools/flow/src/keys.rs
@@ -2,21 +2,17 @@
 
 use std::collections::BTreeMap;
 
-use aes_gcm::{Aes128Gcm, KeyInit, Nonce, AeadInPlace};
-use aes_kw::{KekAes128, Kek};
+use aes_gcm::{AeadInPlace, Aes128Gcm, KeyInit, Nonce};
+use aes_kw::{Kek, KekAes128};
 use anyhow::anyhow;
 use base64::{engine::general_purpose, Engine};
 use coset::{
-    cbor::value::Value,
-    iana,
-    Label,
-    CborSerializable,
-    CoseKdfContextBuilder,
-    SuppPubInfo,
-    RegisteredLabelWithPrivate,
+    cbor::value::Value, iana, CborSerializable, CoseEncrypt, CoseEncryptBuilder,
+    CoseKdfContextBuilder, CoseRecipientBuilder, Header, HeaderBuilder, Label, ProtectedHeader,
+    RegisteredLabelWithPrivate, SuppPubInfo,
 };
-use p256::{SecretKey, PublicKey};
-use rand_core::{CryptoRng, RngCore, OsRng};
+use p256::{elliptic_curve::sec1::ToEncodedPoint, PublicKey, SecretKey};
+use rand_core::{CryptoRng, OsRng, RngCore};
 
 use crate::{data::Example, Result};
 
@@ -74,7 +70,9 @@ impl Key {
             None => return Err(anyhow!("No info entry -1 in recipient unprotected header")),
             Some(m) => m,
         };
-        let info = info.as_map().expect("Unexpected type of 'rest' field in map");
+        let info = info
+            .as_map()
+            .expect("Unexpected type of 'rest' field in map");
 
         // The key type must be a 2, for "EC2".
         match cbor_map_get(info, &Value::Integer(From::from(1i32))) {
@@ -129,7 +127,8 @@ impl Key {
             .build();
         let ctx = ctxb.to_vec().unwrap();
 
-        let secret = p256::ecdh::diffie_hellman(self.secret.to_nonzero_scalar(), eph_pub.as_affine());
+        let secret =
+            p256::ecdh::diffie_hellman(self.secret.to_nonzero_scalar(), eph_pub.as_affine());
         let hkdf = secret.extract::<sha2::Sha256>(None);
         let mut hkey = vec![0u8; 16];
         hkdf.expand(&ctx, &mut hkey).unwrap();
@@ -137,7 +136,8 @@ impl Key {
         // Use AES-KW to unwrap the key.
         let unwr: KekAes128 = Kek::try_from(hkey.as_slice()).unwrap();
         let mut cek = vec![0u8; 16];
-        unwr.unwrap(recipient.ciphertext.as_ref().unwrap().as_slice(), &mut cek).unwrap();
+        unwr.unwrap(recipient.ciphertext.as_ref().unwrap().as_slice(), &mut cek)
+            .unwrap();
 
         // Use this key to build the aead handler for this.
         let cipher = Aes128Gcm::new_from_slice(&cek).unwrap();
@@ -157,11 +157,129 @@ impl Key {
     }
 
     /// Given some payload, and a recipient (where we know their public key), construct a cose packet.
-    pub fn encrypt_cose(&self, plaintext: &[u8], recipient: &PublicKey, mut rng: impl RngCore + CryptoRng) -> Result<Vec<u8>> {
-        let mut session_key = vec![0u8; 16];
-        rng.fill_bytes(&mut session_key);
-        println!("session key: {:02x?}", session_key);
-        unimplemented!()
+    pub fn encrypt_cose(
+        &self,
+        plaintext: &[u8],
+        recipient: &PublicKey,
+        mut rng: impl RngCore + CryptoRng,
+    ) -> Result<Vec<u8>> {
+        let mut cek = vec![0u8; 16];
+        rng.fill_bytes(&mut cek);
+
+        // The kek is the key encryption key, which is also just generated.
+        let mut kek = vec![0u8; 16];
+        rng.fill_bytes(&mut kek);
+
+        // Generate an IV.
+        let mut kw_iv = vec![0u8; 12];
+        rng.fill_bytes(&mut kw_iv);
+
+        // There is an ephemeral key as well that we use to derive the shared
+        // key we wrap with.
+        let eph_key = SecretKey::random(rng);
+
+        // Extract the x and y values from this, as be_bytes.
+        let eph_pub = eph_key.public_key();
+        // Get the SEC1 encoded point out of this.
+        let encoded = eph_pub.as_affine().to_encoded_point(false);
+        let encoded = encoded.as_bytes();
+        assert_eq!(encoded.len(), 65);
+        assert_eq!(encoded[0], 4);
+        let eph_x = &encoded[1..33];
+        let eph_y = &encoded[33..65];
+
+        // Build a protected header for the recipient as this is needed for the
+        // key derivation context.
+        let prot_hd = Header {
+            alg: Some(RegisteredLabelWithPrivate::Assigned(
+                iana::Algorithm::ECDH_ES_A128KW,
+            )),
+            ..Default::default()
+        };
+        // let prot = ProtectedHeader {
+        //     original_data: None,
+        //     header: prot_hd,
+        // };
+        let item_map = Value::Map(vec![
+            (Value::Integer(From::from(1)), Value::Integer(From::from(2))),
+            (
+                Value::Integer(From::from(-1)),
+                Value::Integer(From::from(1)),
+            ),
+            (Value::Integer(From::from(-2)), Value::Bytes(eph_x.to_vec())),
+            (Value::Integer(From::from(-3)), Value::Bytes(eph_y.to_vec())),
+        ]);
+        let unprot_hd = HeaderBuilder::new()
+            .key_id(b"TODO use real key ID".to_vec())
+            .value(-1, item_map)
+            .build();
+
+        // There is a bit of a catch-22 in building this. The API seems to
+        // suggest using the recipient to build the protected header from the
+        // header, but we need it to build the context before we can encrypt the
+        // data. As such, we build a temporary one, even though there isn't a
+        // builder for this.
+        let prot_full_hd = ProtectedHeader {
+            original_data: None,
+            header: prot_hd.clone(),
+        };
+
+        // Use this to build the aead handler for this.
+        let cipher = Aes128Gcm::new_from_slice(&cek).unwrap();
+        let nonce = Nonce::from_slice(&kw_iv);
+
+        // Use HKDF to derive the secret from this. From our perspective, this
+        // is based on the secret from the ephemeral key, and the public data of
+        // the recipient.
+        let alg = iana::Algorithm::A128KW;
+        let ctxb = CoseKdfContextBuilder::new()
+            .supp_pub_info(SuppPubInfo {
+                key_data_length: 128,
+                protected: prot_full_hd,
+                other: None,
+            })
+            .algorithm(alg)
+            .build();
+        let ctx = ctxb.to_vec().unwrap();
+
+        let secret = p256::ecdh::diffie_hellman(eph_key.to_nonzero_scalar(), recipient.as_affine());
+        let hkdf = secret.extract::<sha2::Sha256>(None);
+        let mut hkey = vec![0u8; 16];
+        hkdf.expand(&ctx, &mut hkey).unwrap();
+
+        // Use AES-KW to wrap the cek.
+        let wr: KekAes128 = Kek::try_from(hkey.as_slice()).unwrap();
+        let mut ceke = vec![0u8; 24];
+        wr.wrap(&cek, &mut ceke).unwrap();
+
+        // Build the recipient.
+        let recip = CoseRecipientBuilder::new()
+            .protected(prot_hd)
+            .unprotected(unprot_hd)
+            .ciphertext(ceke)
+            .build();
+
+        // Build the cose header (the above was the recipient header).
+        let cose_prot_hd = HeaderBuilder::new()
+            .algorithm(iana::Algorithm::ECDH_ES_A128KW)
+            .build();
+        let cose_unprot_hd = HeaderBuilder::new().iv(kw_iv.to_vec()).build();
+
+        // Build all of it.
+        let packet = CoseEncryptBuilder::new()
+            .add_recipient(recip)
+            .protected(cose_prot_hd)
+            .unprotected(cose_unprot_hd)
+            .create_ciphertext(plaintext, &[], move |plain, aad| {
+                let mut result = plain.to_vec();
+                cipher.encrypt_in_place(nonce, aad, &mut result).unwrap();
+                result
+            })
+            .build();
+
+        // println!("Packet: {:#?}", packet);
+
+        Ok(packet.to_vec().unwrap())
     }
 }
 
@@ -230,4 +348,9 @@ fn randomkey() {
     let plain = b"This is some plaintext.";
 
     let cblock = sender.encrypt_cose(plain, &recip_pub, OsRng).unwrap();
+
+    // Decode. This will eventually need to be tagged, but shouldn't be yet.
+    let packet = CoseEncrypt::from_slice(&cblock).unwrap();
+    let plain2 = recip_priv.decrypt_cose(&packet).unwrap();
+    assert_eq!(&plain[..], plain2);
 }
